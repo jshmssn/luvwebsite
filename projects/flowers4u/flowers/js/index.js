b25548567e3b461fd6c0fc9e273d0e178a41de19
jshmssn <joshuamission0903@gmail.com>
2024-12-25 15:00:53 +0800

    removed session

--- projects/flowers4u/flowers/js/index.js
+++ projects/flowers4u/flowers/js/index.js
@@ -1,21 +1,4 @@
 
-// Session check function (add this to profile.html)
-function checkSession() {
-  const userSession = localStorage.getItem('userSession');
-  const sessionTimestamp = localStorage.getItem('sessionTimestamp');
-  
-  // Check if session exists and is not expired (24 hours expiration)
-  const SESSION_DURATION = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
-  
-  if (!userSession || !sessionTimestamp || 
-      Date.now() - parseInt(sessionTimestamp) > SESSION_DURATION) {
-      // Session doesn't exist or has expired
-      localStorage.removeItem('userSession');
-      localStorage.removeItem('sessionTimestamp');
-      window.location.href = "../../index.html";
-  }
-}
-
 const title = document.querySelector('.title')
 const text = 'I have something for you'.split('')
 for (let index = 0; index < text.length; index++) {
